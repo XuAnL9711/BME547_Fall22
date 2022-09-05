460277ebe657c85c6458975f9c6c3be4919b05b6
Xuan Liu <liuxuanlx97@gmail.com>
2022-09-05 12:35:11 -0400

    change interface2

--- in_class_example/Sept_5/interface.py
+++ in_class_example/Sept_5/interface.py
@@ -5,5 +5,16 @@ def interface():
     choice = input("Enter your choice: ")
     if choice=='9':
         return
+
+def interface2():
+    print("My Program")
+    print("Options:")
+    print("9 - Quit")
+    keep_running = True
+    while keep_running:
+        choice = input("Enter your choice: ")
+        if choice=='9':
+            return
    
-interface()
+# interface()
+interface2()
