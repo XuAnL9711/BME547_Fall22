a0589e58b8fb3d251997452b8383068707203897
Xuan Liu <liuxuanlx97@gmail.com>
2022-09-05 12:54:31 -0400

    add HDL function

--- in_class_example/Sept_5/interface.py
+++ in_class_example/Sept_5/interface.py
@@ -15,6 +15,23 @@ def interface2():
         choice = input("Enter your choice: ")
         if choice=='9':
             return
-   
+
+def input_HDL():
+    HDL_input = input('Enter the HDL value:')
+    return int(HDL_input)
+
+def check_HDL(HDL_value):
+    if HDL_value >= 60:
+        return "Normal"
+    elif 40 <= HDL_value < 60:
+        return "Borderline Low"
+    elif HDL_value < 40:
+        return "Low"
+
+
 # interface()
-interface2()
+# interface2()
+
+# HDL_input
+
+check_HDL()
